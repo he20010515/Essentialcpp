27e59debfabc83ac40a6bb575487a97e09b1e66b
heyuwei <he20010515@163.com>
2022-12-23 23:55:12 +0800

    update

--- src/chapter3_generic/c_13_desigin_a_filter.cpp
+++ src/chapter3_generic/c_13_desigin_a_filter.cpp
@@ -2,13 +2,14 @@
  * @Author: heyuwei he20010515@163.com
  * @Date: 2022-12-23 20:50:48
  * @LastEditors: heyuwei he20010515@163.com
- * @LastEditTime: 2022-12-23 21:25:47
+ * @LastEditTime: 2022-12-23 23:53:45
  * @FilePath: /Essentialcpp/src/chapter3_generic/c_13_desigin_a_filter.cpp
  * @Description:
  */
 
 #include <vector>
 #include <iostream>
+#include <functional>
 
 template <typename IterType>
 void display(IterType begin, IterType end)
@@ -49,16 +50,64 @@ bool greater_than(int a, int b) { return a > b; }
 bool less_than(int a, int b) { return a < b; }
 bool equal(int a, int b) { return a == b; }
 
+bool less_than_10(int a) { return a < 10; }
+
+int count_occurs(const std::vector<int> &vec, int val)
+{
+    auto iter = vec.begin();
+    int occurs_count = 0;
+    while ((iter = std::find(iter, vec.end(), val)) != vec.end())
+    {
+        ++occurs_count;
+        ++iter;
+    }
+    return occurs_count;
+}
+
+template <typename Iterator, typename Filter>
+Iterator my_filter(Iterator begin,
+                   Iterator end,
+                   Iterator it,
+                   Filter _filter)
+{
+    for (; begin != end; begin++)
+        if (_filter(*begin))
+        {
+            *it = *begin;
+            it++;
+        }
+    return it;
+}
+
 int main(int argc, char const *argv[])
 {
-    int i_array[13] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
-    std::vector<int> i_vector(i_array, i_array + 13);
+    int i_array[16] = {1, 2, 3, 3, 3, 4, 5, 6, 7, 3, 8, 9, 10, 11, 12, 13};
+    std::vector<int> i_vector(i_array, i_array + 16);
     auto v1 = version_1_bigger_than_10(i_vector.begin(), i_vector.end());
     display(v1);
     auto v2 = version_2_filter(i_vector.begin(), i_vector.end(), equal, 3);
     display(v2);
     auto v2_1 = version_2_filter(i_vector.begin(), i_vector.end(), less_than, 3);
     display(v2_1);
+    std::cout << count_occurs(i_vector, 3) << std::endl;
 
+    // function object ,defined in <functional>
+
+    auto c = std::plus<int>();
+    std::cout << "auto c = std::plus<int>(); \nstd::cout << c(1, 2) << std::endl;" << std::endl;
+    std::cout << c(1, 2) << std::endl;
+
+    std::cout << std::plus<std::string>()("hello", " world") << std::endl;
+    std::cout << std::minus<int>()(5, 3) << std::endl;
+    std::cout << std::multiplies<int>()(3, 3) << std::endl;
+    std::cout << std::divides<float>()(4.2, 8.4) << std::endl;
+    std::cout << std::negate<int>()(42) << std::endl;
+
+    std::vector<int> result(10);
+    std::vector<int>::iterator ans = my_filter(i_vector.begin(),
+                                               i_vector.end(),
+                                               result.begin(),
+                                               std::binder2nd<std::less<int>>(std::less<int>(), 5));
+    display(result);
     return 0;
 }
